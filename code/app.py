d5c95da5440a4087f74069f69995f6186a43f286
Nikhil Bokade <68290121+bokadenikhil@users.noreply.github.com>
2023-03-25 18:37:30 +0530

    Update app.py

--- code/app.py
+++ code/app.py
@@ -24,6 +24,6 @@ with col3:
     ratio = st.slider('H2HC Ratio - Mol/Mol', min_value=2, max_value=5, value=4)
 
 if st.button('Predict'):
-    rf = joblib.load(open(r'model_v1.sav','rb'))
+    rf = joblib.load(r'model_v1.sav')
     result = rf.predict(np.array([[ff, r1T, r2T, r3T,PST, ratio]]))
     st.text(result[0])
