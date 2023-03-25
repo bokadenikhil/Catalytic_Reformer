3e7f08ba826c45cc3a66d811e322f4522db661e9
Nikhil Bokade <68290121+bokadenikhil@users.noreply.github.com>
2023-03-25 19:01:26 +0530

    Update app.py

--- code/app.py
+++ code/app.py
@@ -25,6 +25,7 @@ with col3:
     ratio = st.slider('H2HC Ratio - Mol/Mol', min_value=2, max_value=5, value=4)
 
 if st.button('Predict'):
+    st.text(os.getcwd())
     try:
         rf = joblib.load(r'Catalytic_Reformer/code/model_v1.sav')
         result = rf.predict(np.array([[ff, r1T, r2T, r3T,PST, ratio]]))
